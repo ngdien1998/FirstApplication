b0a3edf7bd457fa089108373b7d6c50d2dd01fdd
Nguyen Le Dien <ngdien1998@gmail.com>
2019-02-26 21:37:01 +0700

    Nguyen Le Dien commit: modify RegisterActivity.java

--- app/src/main/java/com/mobileprograming/g4/dien/myapplication/RegisterActivity.java
+++ app/src/main/java/com/mobileprograming/g4/dien/myapplication/RegisterActivity.java
@@ -1,7 +1,6 @@
 package com.mobileprograming.g4.dien.myapplication;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
